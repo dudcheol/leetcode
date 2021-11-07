cb11c2ea64449a65edc2fcd3d13d7103cc8ca3c8
DUDCHEOL <95_pyc@naver.com>
2021-11-07 23:35:00 +0900

    Time: 72 ms (83.33%), Space: 38.9 MB (57.21%) - LeetHub

--- spiral-matrix-ii/spiral-matrix-ii.js
+++ spiral-matrix-ii/spiral-matrix-ii.js
@@ -7,14 +7,10 @@ const dy=[0,1,0,-1];
 const dx=[1,0,-1,0];
 var generateMatrix = function(n) {
     const ary = Array(n);
-    for(let i=0;i<n;i++){
-        ary[i] = Array(n);
-    }
+    for(let i=0;i<n;i++) ary[i] = Array(n);
     
     const fin = n*n;
-    let y=0,x=-1;
-    let d=0;
-    num=1;
+    let y=0,x=-1,d=0,num=1;
     while(true){
         if(num>fin) break;
         y+=dy[d];
