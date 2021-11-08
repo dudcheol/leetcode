39d591ff3d676a471fc135cbf2e61855167cd4cb
DUDCHEOL <95_pyc@naver.com>
2021-11-08 23:05:51 +0900

    Time: 88 ms (57.75%), Space: 40.3 MB (82.56%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.js
+++ merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -11,28 +11,33 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    let node = null;
-    let t1 = l1, t2 = l2;
-    let tmp = [];
-    while(t1){
-        tmp.push(t1.val);
-        t1 = t1.next;
-    }
-    while(t2){
-        tmp.push(t2.val);
-        t2 = t2.next;
-    }
-    tmp = tmp.sort((a,b)=>(a-b));
+    let answer = new ListNode();
+    let tmp = answer;
+    let t1=l1, t2=l2;
     
-    if(!tmp.length) return null;
-    
-    const answer = new ListNode(tmp[0], null);
-    let target = answer;
-    
-    for(let i=1;i<tmp.length;i++){
-        target.next = new ListNode(tmp[i], null);
-        target = target.next;
+    while(t1 && t2){
+        if(t1 && t2){
+            if(t1.val > t2.val){
+                tmp.next = t2; // tmp.next에 t2 객체의 주소를 할당
+                t2 = t2.next; // t2에 t2.next 객체의 주소를 할당
+                // 객체 내부를 건드리는 것이 아니라 할당된 주소를 바꾸는 형태이므로
+                // t2에 t2.next의 주소를 할당한다고 해서 tmp.next가 변하는 것은 아님
+                // 만약, t2 객체 내부를 건드렸다면 tmp.next 객체의 내부도 변할 것임
+            } else {
+                tmp.next = t1;
+                t1 = t1.next;
+            }
+            tmp = tmp.next;
+        }
     }
     
-    return answer;
+    if(!t1){
+        tmp.next = t2;
+    }
+        
+    if(!t2){
+        tmp.next = t1;
+    }
+    
+    return answer.next;
 };
